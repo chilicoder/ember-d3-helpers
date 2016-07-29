a919b03d87122291b4d2f84e151d265d51f59ab0
Matthew Rosenberg <matt@dynamicwebsolutions.com>
2016-07-29 08:07:38 -0400

    moved items to component

--- tests/dummy/app/components/x-chart-area.js
+++ tests/dummy/app/components/x-chart-area.js
@@ -1,8 +1,9 @@
 import Ember from 'ember';
 import { select } from 'd3-selection';
+const { Component, computed, run } = Ember;
 
 
-export default Ember.Component.extend({
+export default Component.extend({
 
   tagName: 'svg',
 
@@ -13,13 +14,47 @@ export default Ember.Component.extend({
   data: null,
 
 
-  attributeBindings: [ 'width', 'height' ],
+  attributeBindings: [ 'containerWidth', 'containerHeight', 'viewBox' ],
 
 
   width: null,
 
 
   height: null,
+
+
+  viewBox: computed( 'conatinerHeight', 'containerWidth', function() {
+    const width  = this.get( 'containerWidth' );
+    const height = this.get( 'containerHeight' );
+
+    return [ 0, 0, width, height ];
+  }),
+
+
+  containerWidth: computed( 'width', 'margin.right', 'margin.left', function() {
+    const width = this.get( 'width' );
+    const left  = this.get( 'margin.left' );
+    const right = this.get( 'margin.right' );
+
+    return width + left + right;
+  }),
+
+
+  containerHeight: computed( 'height', 'margin.top', 'margin.bottom', function() {
+    const height = this.get( 'height' );
+    const top    = this.get( 'margin.top' );
+    const bottom = this.get( 'margin.bottom' );
+
+    return height + top + bottom;
+  }),
+
+
+  margin: {
+    'top':10,
+    'right':10,
+    'bottom':10,
+    'left':30
+  },
 
 
   xAccessor( d ) {
@@ -32,15 +67,9 @@ export default Ember.Component.extend({
   },
 
 
-  idAccessor( d, i ) {
-    debugger;
-    return d ? d : this.id;
-  },
-
-
   didInsertElement() {
     this._super( ...arguments );
-    Ember.run.scheduleOnce( 'afterRender', this, 'renderChart' )
+    run.scheduleOnce( 'afterRender', this, 'renderChart' )
   },
 
 
