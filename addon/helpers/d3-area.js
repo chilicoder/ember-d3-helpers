1cf89b818b5e65f883156a7eee1a897bea8e94d6
Matthew Rosenberg <matt@dynamicwebsolutions.com>
2016-08-01 09:05:35 -0400

    working cahrt with axis

--- addon/helpers/d3-area.js
+++ addon/helpers/d3-area.js
@@ -1,9 +1,9 @@
 import Ember from 'ember';
 import { area } from 'd3-shape';
 
-export function d3Area( [ xScale, yScale ], { xAccessor, yAccessor } ) {
-  let xFn  = (d) => xScale( xAccessor( d ) );
-  let y1Fn = (d) => yScale( yAccessor( d ) );
+export function d3Area( [ xScale, yScale ], { xKey, yKey } ) {
+  let xFn  = (d) => xScale( xKey( d ) );
+  let y1Fn = (d) => yScale( yKey( d ) );
   let y0Fn = ()  => yScale( 0 );
 
   return area().x( xFn ).y1( y1Fn ).y0( y0Fn );
