4fdc6ce2cc0105868f0f371dcf13777c1280d3a7
Matthew Rosenberg <matt@dynamicwebsolutions.com>
2016-08-01 09:05:35 -0400

    working cahrt with axis

--- tests/dummy/app/controllers/area-chart.js
+++ tests/dummy/app/controllers/area-chart.js
@@ -1,6 +1,8 @@
 import Ember from 'ember';
+import Axis from 'd3-axis';
+const { Controller, computed } = Ember;
 
-export default Ember.Controller.extend({
+export default Controller.extend({
 
 
   text: "Why do you write like you're writing out of time",
@@ -9,6 +11,67 @@ export default Ember.Controller.extend({
   height: 400,
 
 
-  width: 600
+  width: 600,
+
+
+  margin: {
+    'top':10,
+    'right':10,
+    'bottom':20,
+    'left':20
+  },
+
+
+  containerWidth: computed( 'width', 'margin.right', 'margin.left', function() {
+    const width = this.get( 'width' );
+    const left  = this.get( 'margin.left' );
+    const right = this.get( 'margin.right' );
+
+    return width + left + right;
+  }),
+
+
+  containerHeight: computed( 'height', 'margin.top', 'margin.bottom', function() {
+    const height = this.get( 'height' );
+    const top    = this.get( 'margin.top' );
+    const bottom = this.get( 'margin.bottom' );
+
+    return height + top + bottom;
+  }),
+
+
+  viewBox: computed( 'conatinerHeight', 'containerWidth', function() {
+    const width  = this.get( 'containerWidth' );
+    const height = this.get( 'containerHeight' );
+
+    return [ 0, 0, width, height ];
+  }),
+
+
+  xKey( d ) {
+    return d.char;
+  },
+
+
+  yKey( d ) {
+    return d.count;
+  },
+
+
+  xDomain( domain ) {
+    return domain.map( d => { return d.char; });
+  },
+
+
+  xAxis( xScale ) {
+    return Axis['axisBottom']( xScale );
+  },
+
+
+  yAxis( yScale ) {
+    return Axis['axisLeft']( yScale );
+  }
+
+
 
 });
