e55b17ec4bdf67194f24f9eaf1d349bb32f475a9
Matthew Rosenberg <matt@dynamicwebsolutions.com>
2016-10-14 14:02:29 -0400

    xkey name cahnge

--- addon/helpers/d3-area.js
+++ addon/helpers/d3-area.js
@@ -1,10 +1,11 @@
 import Ember from 'ember';
 import { area } from 'd3-shape';
 
-export function d3Area( [ xScale, yScale ], { xKey, yKey } ) {
-  let xFn  = (d) => xScale( xKey( d ) );
-  let y1Fn = (d) => yScale( yKey( d ) );
-  let y0Fn = ()  => yScale( 0 );
+export function d3Area( [ xScale, yScale ], { xAccessor, yAccessor } ) {
+  let yMin = yScale.domain()[0];
+  let xFn  = (d) => xScale( xAccessor( d ) );
+  let y1Fn = (d) => yScale( yAccessor( d ) );
+  let y0Fn = ()  => yScale( yMin );
 
   return area().x( xFn ).y1( y1Fn ).y0( y0Fn );
 }
