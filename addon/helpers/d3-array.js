2a6ceb8187885facfa7ec2833edb9674143cf1a2
Ivan Vanderbyl <ivanvanderbyl@users.noreply.github.com>
2016-10-18 07:17:13 +1100

    Switch to ember-d3 now that ember-cli-d3-shape is deprecated (#28)
    * Use ember-d3 instead of d3-shape
    * Fix deprecation notice
    * Use NPM 3
    * Lock to released version for V4 support

--- addon/helpers/d3-array.js
+++ addon/helpers/d3-array.js
@@ -4,7 +4,7 @@ import array from 'd3-array';
 let allowedMethods = Ember.A(['min', 'max', 'extent', 'sum', 'mean', 'median', 'variance', 'deviation', 'scan', 'merge', 'pairs', 'permute']);
 
 export function d3Array([ method, ...args ]) {
-  Ember.assert('this method is not yet supported', allowedMethods.contains(method));
+  Ember.assert('this method is not yet supported', allowedMethods.includes(method));
 
   let arrayMethod = array[method];
   return arrayMethod.apply(null, args);
