e3fa0f7b1b8f287969b0eb683bdd640d768cbfa1
zigahertz <zach.zibrat@locusenergy.com>
2016-11-14 15:19:22 -0800

    adds sanity check tests

--- tests/unit/helpers/d3-format-test.js
+++ tests/unit/helpers/d3-format-test.js
@@ -1,12 +1,10 @@
-
 import { d3Format } from 'dummy/helpers/d3-format';
 import { module, test } from 'qunit';
 
 module('Unit | Helper | d3 format');
 
-// Replace this with your real tests.
 test('it works', function(assert) {
-  let result = d3Format([42]);
-  assert.ok(result);
+  let formatter = d3Format(['.2s']);
+  assert.equal(formatter(4312), '4.3k');
 });
 
--- tests/unit/helpers/d3-time-format-test.js
+++ tests/unit/helpers/d3-time-format-test.js
@@ -1,12 +1,10 @@
-
 import { d3TimeFormat } from 'dummy/helpers/d3-time-format';
 import { module, test } from 'qunit';
 
 module('Unit | Helper | d3 time format');
 
-// Replace this with your real tests.
 test('it works', function(assert) {
-  let result = d3TimeFormat([42]);
-  assert.ok(result);
+  let formatter = d3TimeFormat([ '%B %d, %Y' ]);
+  assert.equal(formatter(new Date('2011-10-02')), 'October 01, 2011');
 });
 
