ac0fda4a15f8f7139aa039cc5d9de942815d4b9c
zigahertz <zach.zibrat@locusenergy.com>
2016-11-14 15:43:39 -0800

    fixes test

--- tests/unit/helpers/d3-time-format-test.js
+++ tests/unit/helpers/d3-time-format-test.js
@@ -4,7 +4,9 @@ import { module, test } from 'qunit';
 module('Unit | Helper | d3 time format');
 
 test('it works', function(assert) {
-  let formatter = d3TimeFormat([ '%B %d, %Y' ]);
-  assert.equal(formatter(new Date('2011-10-02')), 'October 01, 2011');
+  let formatter = d3TimeFormat([ '%Y' ]);
+  
+  // this is just a sanity check - lets ensure we don't have to deal with TZ / DST issues in CI 
+  assert.equal(formatter(new Date('2011-08-02')), '2011');
 });
 
