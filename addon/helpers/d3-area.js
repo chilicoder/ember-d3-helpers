b9148687a57b60f32f5e04a53d330c51cb13420c
Matthew Rosenberg <matt@dynamicwebsolutions.com>
2016-11-21 16:40:28 -0500

    reformat area helper to match locus style, remove are demo link

--- addon/helpers/d3-area.js
+++ addon/helpers/d3-area.js
@@ -3,11 +3,11 @@ import { area } from 'd3-shape';
 
 export function d3Area( [ xScale, yScale ], { xAccessor, yAccessor } ) {
   let yMin = yScale.domain()[0];
-  let xFn  = (d) => xScale( xAccessor( d ) );
-  let y1Fn = (d) => yScale( yAccessor( d ) );
-  let y0Fn = ()  => yScale( yMin );
+  let xFn  = (d) => xScale(xAccessor(d));
+  let y1Fn = (d) => yScale(yAccessor(d));
+  let y0Fn = ()  => yScale(yMin);
 
-  return area().x( xFn ).y1( y1Fn ).y0( y0Fn );
+  return area().x(xFn).y1(y1Fn).y0(y0Fn);
 }
 
 export default Ember.Helper.helper(d3Area);
