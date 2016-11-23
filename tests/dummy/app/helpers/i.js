97180a466864d3003cee6a89a5c1636fb60c74e9
Matthew Rosenberg <matt@dynamicwebsolutions.com>
2016-11-23 07:19:29 -0500

    remove vestigal helper

--- tests/dummy/app/helpers/i.js
+++
@@ -1,9 +0,0 @@
-import Ember from 'ember';
-
-const matcher = /\$\{\s*(.*?)\s*\}/g;
-
-export function interpolate([ str ], hash) {
-  return str.replace(matcher,  (i, match) => Ember.get(hash, match));
-}
-
-export default Ember.Helper.helper(interpolate);
